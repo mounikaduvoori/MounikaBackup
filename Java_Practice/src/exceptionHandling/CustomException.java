31c5835aa33b60018abcebb704805590ce29ddf0
gitpractice9 <githubpractice@gmail.com>
2016-02-23 10:33:32 +0530

    Sample programs related to exception handling in java

--- Java_Practice/src/exceptionHandling/CustomException.java
+++ Java_Practice/src/exceptionHandling/CustomException.java
@@ -19,6 +19,7 @@ import java.io.*;
 		  }  		  
 }
  
+ 
 /*If you are creating your own Exception that is known as custom exception or 
  user-defined exception. Java custom exceptions are used to customize the exception 
  according to user need.By the help of custom exception, you can have your own exception and message.
